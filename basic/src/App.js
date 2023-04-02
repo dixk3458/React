cbed14d65fe27db49e31871ae9840d4f082499ed
dixk3458 <dixk3458@naver.com>
2023-04-02 19:18:40 +0900

    jsx 문법 연습 및 공부

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '정재웅';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <h2 style={{ background: 'blue' }}>{name}</h2>
+
+      <ul>
+        <li>사과</li>
+        <li>수박</li>
+        <li>포도</li>
+      </ul>
     </div>
   );
 }
