65c11fecb0a2031968fda266d68e44803e1352bf
dixk3458 <dixk3458@naver.com>
2023-04-04 21:38:21 +0900

    profile components

---
+++ basic/src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <img
+        className="photo"
+        src="https://lh3.googleusercontent.com/e_kBO10kPf4shhLHUsfFwCcTkmggoQ5KiCE1bZWd78x9ly7U0Qinj5ECuT7Gnmazvr2gNM2QhiHNqg9_Bi2SD_FtOepftmZ99hJzQFXhpQ"
+        alt="avatar"
+      />
+      <h1>프론트엔드 개발자 지망생</h1>
+      <p className="name">정재웅</p>
+    </div>
+  );
+}
