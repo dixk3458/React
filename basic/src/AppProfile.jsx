ed4340bec495fea6b487e373cfa727430f1bfa48
dixk3458 <dixk3458@naver.com>
2023-04-05 21:21:13 +0900

    재사용성이 뛰어난 컴포넌트

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -5,8 +5,20 @@ import './App.css';
 export default function AppProfile() {
   return (
     <div>
-      <Profile />
-      <Profile />
+      <Profile
+        image={
+          'https://lh3.googleusercontent.com/71taw1bTsnXa4WJpxkn8TbyQD9L_Ha4_4WOYKGKiz80GF4GotWNBBTwVKmOKq_lWw6aiR24lo_QbXeVleIDG7SBr6Pdr9gwENQkBgKyUxg'
+        }
+        name={'정재웅'}
+        title={'프론트엔드 개발자'}
+      />
+      <Profile
+        image={
+          'https://search.pstatic.net/common?type=b&size=336&expire=1&refresh=true&quality=100&direct=true&src=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFs8bu_WacAA8ee1.jpg'
+        }
+        name={'지수'}
+        title={'블랙핑크'}
+      />
     </div>
   );
 }
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-export default function Profile() {
+export default function Profile({ image, name, title }) {
   return (
     <div className="profile">
-      <img
-        className="photo"
-        src="https://lh3.googleusercontent.com/e_kBO10kPf4shhLHUsfFwCcTkmggoQ5KiCE1bZWd78x9ly7U0Qinj5ECuT7Gnmazvr2gNM2QhiHNqg9_Bi2SD_FtOepftmZ99hJzQFXhpQ"
-        alt="avatar"
-      />
-      <h1>프론트엔드 개발자 지망생</h1>
-      <p className="name">정재웅</p>
+      <img className="photo" src={image} alt="avatar" />
+      <h1 className="name">{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
